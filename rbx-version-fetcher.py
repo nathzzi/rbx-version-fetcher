ba303f3461f34c2e24c8c2e0901e0d0cff896fda
Nathan <100944663+nathzzi@users.noreply.github.com>
2025-06-27 03:42:43 +0800

    Added Roblox VNG version support

--- rbx-version-fetcher.py
+++ rbx-version-fetcher.py
@@ -1,12 +1,15 @@
 # Created by Nathan on June 26, 2025
 # Thanks to APKCombo for making this possible
 
+# Changelogs 06/27/25
+# - Added VNG version support
+
 import requests
 from bs4 import BeautifulSoup
 import re
 
-def fetchRobloxVersion():
-    url = "https://apkcombo.com/roblox/com.roblox.client/"
+def fetchRobloxVersion(url_suffix):
+    url = f"https://apkcombo.com/{url_suffix}/"
     headers = {
         "User-Agent": "Mozilla/5.0"
     }
@@ -17,7 +20,7 @@ def fetchRobloxVersion():
     links = soup.find_all("a", href=True)
     for link in links:
         href = link["href"]
-        if href.endswith("/roblox/com.roblox.client/download/apk"):
+        if href.endswith("/download/apk"):
             text = link.get_text(strip=True)
             match = re.match(r"([\d\.]+)\s*\((\d+)\)", text)
             if match:
@@ -27,6 +30,12 @@ def fetchRobloxVersion():
     return None, None
 
 if __name__ == "__main__":
-    version, version_code = fetchRobloxVersion()
-    print("Latest roblox version fetched:", version)
-    print("Latest roblox version code fetched:", version_code)
+    global_version, global_code = fetchRobloxVersion("roblox/com.roblox.client")
+    print(f"{'Latest Roblox Global version fetched:'} {global_version}")
+    print(f"{'Latest Roblox Global version code fetched:'} {global_code}")
+
+    print(f"{'-' * 50}")
+
+    vn_version, vn_code = fetchRobloxVersion("roblox-vn/com.roblox.client.vnggames")
+    print(f"{'Latest Roblox VNG version fetched:'} {vn_version}")
+    print(f"{'Latest Roblox VNG version code fetched:'} {vn_code}")
